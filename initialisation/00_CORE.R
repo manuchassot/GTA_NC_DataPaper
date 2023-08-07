883c00c631e08b489210deebd5f70a97560d36fc
Emmanuel Chassot <EChassot@iotc.local>
2023-08-07 13:08:44 +0400

    Addition of comment

--- initialisation/00_CORE.R
+++ initialisation/00_CORE.R
@@ -7,6 +7,7 @@ source("91_COLOR_LAYOUT.R")
 source("92_MAP_PACIFIC_PROJECTION_FUNCTION.R")
 
 # Core charts and tables
+# To run the first time so as to get most recent code lists from GH
 #source("01.1_DOWNLOAD_CODE_LISTS.R")
 #source("01.2_DOWNLOAD_MAPPINGS.R")
 source("01.3_LOAD_CODE_LIST_MAPPINGS.R")

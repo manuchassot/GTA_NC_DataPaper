7ddd248d318dd595817838c7e300f3cd49edd6c5
Emmanuel Chassot <EChassot@iotc.local>
2023-08-08 12:55:25 +0400

    Inclusion of download script for the spatial layers

--- initialisation/00_CORE.R
+++ initialisation/00_CORE.R
@@ -7,9 +7,10 @@ source("91_COLOR_LAYOUT.R")
 source("92_MAP_PACIFIC_PROJECTION_FUNCTION.R")
 
 # Core charts and tables
-# To run the first time so as to get most recent code lists from GH
-#source("01.1_DOWNLOAD_CODE_LISTS.R")
-#source("01.2_DOWNLOAD_MAPPINGS.R")
+# To run the first time so as to get most recent files
+#source("00.1_DOWNLOAD_CODE_LISTS.R")
+#source("00.2_DOWNLOAD_MAPPINGS.R")
+#source("00.3_DOWNLOAD_SPATIAL_LAYERS.R")
 source("01.3_LOAD_CODE_LIST_MAPPINGS.R")
 source("01.4_LOAD_SPATIAL_LAYERS.R")
 source("02_LOAD_NC_DATA.R")
--- initialisation/02_LOAD_NC_DATA.R
+++ initialisation/02_LOAD_NC_DATA.R
@@ -4,6 +4,9 @@ NC_RAW = fread("../inputs/data/global_nominal_catch_firms_level0.csv")
 # Append taxonomic information
 NC = merge(NC_RAW, SPECIES_ITIS[, .(species_group_gta = `Species group`, species_code_asfis = `ASFIS code`, taxon = `Scientific name`, species_aggregate = Aggregate, tsn = TSN)], by.x = "species", by.y = "species_code_asfis", all.x = TRUE)
 
+# Append species English name
+NC = merge(NC, SPECIES_LEVEL0[, .(code, species_name = name_en)], by.x = "species", by.y = "code", all.x = TRUE)
+
 # Append gear information
 NC = merge(NC, GEARS[, .(code, gear_label = label)], by.x = "gear_type", by.y = "code", all.x = TRUE)
 

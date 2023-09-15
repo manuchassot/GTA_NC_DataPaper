06396150a118b1987421839b2038f36cd5e0f962
manuchassot <manu.chassot@gmail.com>
2023-09-15 12:38:57 +0400

    Filtering of FSJ data on tuna and shark/ray species

--- initialisation/03_LOAD_FSJ_DATA.R
+++ initialisation/03_LOAD_FSJ_DATA.R
@@ -1,10 +1,11 @@
 print("Reading FAO catches...")
 
 # Read the catch data
-CAPTURE_RAW = fread("../inputs/data/Capture_Quantity.csv")
+CAPTURE_RAW = fread("../inputs/data/FSJ/Capture_Quantity.csv")
 
 # Build the full data set with code lists
 CAPTURE = CAPTURE_RAW[CL_COUNTRIES, on = c("COUNTRY.UN_CODE" = "UN_CODE"), nomatch = 0][CL_SPECIES, on = c("SPECIES.ALPHA_3_CODE" = "SPECIES_CODE"), nomatch = 0][CL_AREAS, on = c("AREA.CODE" = "FISHING_GROUND_CODE"), nomatch = 0][CL_UNITS, on = c("MEASURE" = "MEASUREMENT_CODE"), nomatch = 0][, COUNTRY.UN_CODE := NULL]
 
-
+# Filter on tuna and tuna-like species and sharks and rays
+CAPTURE = CAPTURE[ISSCAAP_NAME %in% c("Tunas, bonitos, billfishes", "Sharks, rays, chimaeras")]
 

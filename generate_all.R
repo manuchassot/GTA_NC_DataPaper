e6077af5b6325a4b03466fb018ae03811494d11b
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-09-18 10:40:58 +0400

    minor updates

--- generate_all.R
+++ generate_all.R
@@ -11,12 +11,15 @@ library(officedown)
 library(officer)
 library(rmarkdown)
 
-
+require(here)
 # Run the R scripts 
 source(here("initialisation/00_CORE.R"))
 
 # DOCX
 # see YAML params here: https://davidgohel.github.io/officedown/reference/rdocx_document.html
+
+source(knitr::purl(here("rmd/setup_markdown.Rmd"), quiet=TRUE))
+
 render(here("rmd/00_ALL.Rmd"), 
        output_dir    = here("outputs/"), 
        output_file   = here("GTA_Nominal_Catch_Data_Paper.docx")

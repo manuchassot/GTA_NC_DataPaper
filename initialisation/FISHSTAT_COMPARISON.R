11aa13877c06ec8cacc95936fd7c124cba56d11d
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-09-18 10:41:30 +0400

    here

--- initialisation/FISHSTAT_COMPARISON.R
+++ initialisation/FISHSTAT_COMPARISON.R
@@ -1,9 +1,9 @@
 ## Those files will be used in the comparison file
 
-dir.create(paste0("rmd/outputs/Comp_FS_NC/figures"), recursive = TRUE, showWarnings = FALSE)
+dir.create(here("rmd/outputs/Comp_FS_NC/figures"), recursive = TRUE, showWarnings = FALSE)
 
 
-parameters_child_global <- list(fig.path = paste0("outputs/Comp_FS_NC/figures/"), 
+parameters_child_global <- list(fig.path = here("rmd/outputs/Comp_FS_NC/figures/"), 
                                 print_map = FALSE, time_dimension = "year", 
                                 unique_analyse = FALSE, 
                                 parameter_init =here("inputs/data/comparison_Fishstat_NC/Fishstat"), 
@@ -25,6 +25,21 @@ rmarkdown::render(here("rmd/comparison.Rmd"),
                   output_file = paste0("Recap.html"),
                   output_dir = here("rmd/outputs/Comp_FS_NC"))
 
+#####################
 
+# filtering_param <-  readRDS(here("inputs/data/comparison_Fishstat_NC/communtime_species.rds"))
+
+dir.create(here("rmd/outputs/Comp_FS_NC_FILTERED/figures"), recursive = TRUE, showWarnings = FALSE)
+
+parameters_child_global$parameter_init =here("inputs/data/comparison_Fishstat_NC/Fishstatfiltered")
+parameters_child_global$parameter_final = here("inputs/data/comparison_Fishstat_NC/NCfiltered")
+parameters_child_global$fig.path = paste0(here("rmd/outputs/Comp_FS_NC_FILTERED/figures/"))
+child_env_global = new.env()
+list2env(parameters_child_global, env = child_env_global)
+
+rmarkdown::render(here("rmd/comparison.Rmd"), 
+                  envir =  child_env_global, 
+                  output_file = paste0("Comp_FS_NC_filtered.html"),
+                  output_dir = here("rmd/outputs/Comp_FS_NC_FILTERED"))
 
 

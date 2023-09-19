8d87e92b37601efd32d389d67edd92f48a8028a4
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-09-19 13:44:16 +0400

    merging issues

--- initialisation/01.1_LOAD_GTA_CODE_LISTS.R
+++ initialisation/01.1_LOAD_GTA_CODE_LISTS.R
@@ -11,12 +11,9 @@ SPECIES_LEVEL0 = fread(here("inputs/codelists/cl_species_level0.csv"))
 # ITIS taxonomic information
 SPECIES_ITIS = fread(here("inputs/codelists/cl_itis_species.csv"))
 
-<<<<<<< HEAD:initialisation/01.3_LOAD_CODE_LIST_MAPPINGS.R
 # SOURCE AUTHORITY - SPECIES
 SOURCE_AUTHORITY_SPECIES = fread(here("inputs/mappings/codelist_mapping_source_authority_species.csv"))
 
-=======
->>>>>>> 89165a34f78cfb406a7db34d5bc6d97a32f175e0:initialisation/01.1_LOAD_GTA_CODE_LISTS.R
 # GTA species groups
 SPECIES_GROUP = fread(here("inputs/codelists/cl_speciesgroup.csv"))
 
@@ -26,16 +23,13 @@ GEARS = fread(here("inputs/codelists/cl_isscfg_gear.csv"), colClasses = "charact
 ## FIRMS fleets ####
 FLEETS = fread(here("inputs/codelists/cl_fishing_fleet.csv"), encoding = "UTF-8")
 
-<<<<<<< HEAD:initialisation/01.3_LOAD_CODE_LIST_MAPPINGS.R
 ## GTA SPECIES GROUPS MAPPING ####
 MAPPING_SP_SPG = fread(here("inputs/mappings/codelist_mapping_species_asfis_speciesgroup_tunaatlas.csv"))
 
 # Add species group label
 MAPPING_SP_SPG = merge(MAPPING_SP_SPG, SPECIES_GROUP[, .(code, species_group_label = label)], by.x = "trg_code", by.y = "code")
-=======
 ## FAO countries ####
 # https://data.iotc.org/reference/latest/domain/admin/#countries
 COUNTRIES = fread("../inputs/codelists/cl_countries.csv")
->>>>>>> 89165a34f78cfb406a7db34d5bc6d97a32f175e0:initialisation/01.1_LOAD_GTA_CODE_LISTS.R
 
 print("Code list files loaded!")

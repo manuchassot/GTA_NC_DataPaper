65493116b0f82b40a192eba845b3ba5d05032d6d
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-09-19 13:44:58 +0400

    merging issues

--- initialisation/01.3_LOAD_GTA_SPATIAL_LAYERS.R
+++ initialisation/01.3_LOAD_GTA_SPATIAL_LAYERS.R
@@ -4,10 +4,6 @@ print("Loading the spatial layers...")
 WORLD_BORDERS_SF = ne_countries(returnclass = "sf")
 
 ## Countries and tRFMO areas
-<<<<<<< HEAD:initialisation/01.4_LOAD_SPATIAL_LAYERS.R
-base::load("inputs/spatial_layers/SpatialLayers.RData")
-=======
-base::load("../inputs/spatial_layers/SpatialLayers.RData")
->>>>>>> 89165a34f78cfb406a7db34d5bc6d97a32f175e0:initialisation/01.3_LOAD_GTA_SPATIAL_LAYERS.R
+base::load(here("inputs/spatial_layers/SpatialLayers.RData"))
 
 print("Spatial layers loaded!")

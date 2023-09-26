3686f612e268d8456242949a5ccd8f37e31b7a36
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-09-26 17:27:46 +0400

    Adding a comparison by Ocean bassin

--- initialisation/06_COMPARISON_NC_FS.R
+++ initialisation/06_COMPARISON_NC_FS.R
@@ -1,73 +1,73 @@
-
-## Those files will be used in the comparison file
-
-dir.create(here("rmd/Comparison_NC_FS/figures"), recursive = TRUE, showWarnings = FALSE)
-required_packages <- c("webshot",
-                       "here", "usethis","ows4R","sp", "data.table", "flextable", "readtext", "sf", "dplyr", "stringr", "tibble",
-                       "bookdown", "knitr", "purrr", "readxl", "base", "remotes", "utils", "DBI", 
-                       "odbc", "rlang", "kableExtra", "readr", "tidyr", "ggplot2", "stats", "RColorBrewer", 
-                       "cowplot", "RPostgreSQL", "curl", "officer", "gdata", "tidyr", "knitr", "tmap"
-)
-
-for (package in required_packages) {
-  if (!requireNamespace(package, quietly = TRUE)) {
-    install.packages(package, dependencies = TRUE)
-  }
-  library(package, character.only = TRUE)
-}
-
-
-last_path = function(y){tail(str_split(y,"/")[[1]],n=1)}
-
-url_analysis_markdown <- "https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/Analysis_markdown/"
-target_dir <- here("rmd/Comparison_NC_FS")  # Current working directory
-
-copyrmd <- function(x, url_path = url_analysis_markdown) {
-  target_file <- file.path(target_dir, last_path(x))
-  
-  if (!file.exists(target_file)) {
-    download_url <- paste0(url_path, x)
-    download.file(url = download_url, destfile = target_file, mode = "wb")
-  }
-}
-
-# Existing files
-c_existing <- c(
-  "tableau_recap_global_action_effort.Rmd", 
-  "comparison.Rmd", 
-  "strata_conversion_factor_gihtub.Rmd", 
-  "template.tex",
-  "dmk-format.csl", 
-  "setup_markdown.Rmd", 
-  "strata_in_georef_but_no_nominal.Rmd"
-)
-
-# Child Rmd files you want to include
-c_child <- c(
-  "Setup_markdown.Rmd",
-  "Parameters_settings.Rmd",
-  "file_formatting.Rmd",
-  "Explenation.Rmd",
-  "Filtering_data.Rmd",
-  "Groupping_differences.Rmd",
-  "Strataloss.Rmd",
-  "Summarydifferences.Rmd",
-  "Compnumberstratas.Rmd",
-  "Timecoverage.Rmd",
-  "Spatialcoverage.Rmd",
-  "Otherdimensions.Rmd",
-  "Timediff.Rmd",
-  "Geographicdiff.Rmd",
-  "Differences_for_each_dimensions.Rmd",
-  "Recap_without_mapping.Rmd",
-  "Annexe.Rmd", "Functions_markdown.Rmd"
-)
-
-# Combine existing and child Rmd files
-c_all <- c(c_existing, c_child)
-
-# Download all files
-lapply(c_all, copyrmd)
+# 
+# ## Those files will be used in the comparison file
+# 
+# dir.create(here("rmd/Comparison_NC_FS/figures"), recursive = TRUE, showWarnings = FALSE)
+# required_packages <- c("webshot",
+#                        "here", "usethis","ows4R","sp", "data.table", "flextable", "readtext", "sf", "dplyr", "stringr", "tibble",
+#                        "bookdown", "knitr", "purrr", "readxl", "base", "remotes", "utils", "DBI", 
+#                        "odbc", "rlang", "kableExtra", "readr", "tidyr", "ggplot2", "stats", "RColorBrewer", 
+#                        "cowplot", "RPostgreSQL", "curl", "officer", "gdata", "tidyr", "knitr", "tmap"
+# )
+# 
+# for (package in required_packages) {
+#   if (!requireNamespace(package, quietly = TRUE)) {
+#     install.packages(package, dependencies = TRUE)
+#   }
+#   library(package, character.only = TRUE)
+# }
+# 
+# 
+# last_path = function(y){tail(str_split(y,"/")[[1]],n=1)}
+# 
+# url_analysis_markdown <- "https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/Analysis_markdown/"
+# target_dir <- here("rmd/Comparison_NC_FS")  # Current working directory
+# 
+# copyrmd <- function(x, url_path = url_analysis_markdown) {
+#   target_file <- file.path(target_dir, last_path(x))
+#   
+#   if (!file.exists(target_file)) {
+#     download_url <- paste0(url_path, x)
+#     download.file(url = download_url, destfile = target_file, mode = "wb")
+#   }
+# }
+# 
+# # Existing files
+# c_existing <- c(
+#   "tableau_recap_global_action_effort.Rmd", 
+#   "comparison.Rmd", 
+#   "strata_conversion_factor_gihtub.Rmd", 
+#   "template.tex",
+#   "dmk-format.csl", 
+#   "setup_markdown.Rmd", 
+#   "strata_in_georef_but_no_nominal.Rmd"
+# )
+# 
+# # Child Rmd files you want to include
+# c_child <- c(
+#   "Setup_markdown.Rmd",
+#   "Parameters_settings.Rmd",
+#   "file_formatting.Rmd",
+#   "Explenation.Rmd",
+#   "Filtering_data.Rmd",
+#   "Groupping_differences.Rmd",
+#   "Strataloss.Rmd",
+#   "Summarydifferences.Rmd",
+#   "Compnumberstratas.Rmd",
+#   "Timecoverage.Rmd",
+#   "Spatialcoverage.Rmd",
+#   "Otherdimensions.Rmd",
+#   "Timediff.Rmd",
+#   "Geographicdiff.Rmd",
+#   "Differences_for_each_dimensions.Rmd",
+#   "Recap_without_mapping.Rmd",
+#   "Annexe.Rmd", "Functions_markdown.Rmd"
+# )
+# 
+# # Combine existing and child Rmd files
+# c_all <- c(c_existing, c_child)
+# 
+# # Download all files
+# lapply(c_all, copyrmd)
 
 
 
@@ -77,6 +77,7 @@ CAPTURE_TO_COMPARE <- CAPTURE
 
 
 NC_TO_COMPARE <- NC
+NC_TO_COMPARE$measurement_type <- "UNK"
 
 
 NC_TO_COMPARE <- NC_TO_COMPARE %>% left_join(MAPPING_FLEET_FSJ_COUNTRY, by = c("fishing_fleet"= "code")) %>% 
@@ -283,6 +284,34 @@ source(purl(here("rmd/Comparison_NC_FS/Functions_markdown.Rmd")))
 rmarkdown::render(here("rmd/Comparison_NC_FS/comparison.Rmd"), 
                   envir =  child_env_global, 
                   output_dir = here("rmd/Comparison_NC_FS/figures_filtered"))
+
+for (ocean in unique(NC_TO_COMPARE_filtered$ocean_basin)){
+  
+  dir.create(here(paste0("rmd/Comparison_NC_FS/", ocean)), recursive = TRUE)
+  
+  parameters_child_global <- list(fig.path = ocean, 
+                                  print_map = FALSE, parameter_time_dimension = "year", 
+                                  unique_analyse = FALSE, 
+                                  parameter_filtering = list("ocean_basin" = ocean),
+                                  parameter_init =here("inputs/data/comparison_Fishstat_NC/Fishstat_filtered"), 
+                                  parameter_final = here("inputs/data/comparison_Fishstat_NC/NC_filtered"), 
+                                  parameter_colnames_to_keep =c("species", "measurement_type", "year", "measurement_value", 
+                                                                "country_code", "species_group_gta", "taxon", "species_name", 
+                                                                "gear_label", "fleet_label", "ocean_basin", "measurement_unit"),
+                                  parameter_diff_value_or_percent = "Difference in value",
+                                  parameter_fact = "catch", shape_without_geom = NULL, 
+                                  species_group = NULL, child_header = "", 
+                                  parameter_titre_dataset_1 = "Fishstat",
+                                  parameter_titre_dataset_2 = "Nominal catch" )
+  
+  child_env_global <- new.env()
+  list2env(parameters_child_global, child_env_global)
+  
+  source(purl(here("rmd/Comparison_NC_FS/Functions_markdown.Rmd")))
+  rmarkdown::render(here("rmd/Comparison_NC_FS/comparison.Rmd"), 
+                    envir =  child_env_global, 
+                    output_dir = here(paste0("rmd/Comparison_NC_FS/", ocean)))
+}
 
 
 # Temporal differences for each species FS/NC -----------------------------

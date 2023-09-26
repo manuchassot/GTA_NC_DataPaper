846c57a6cb0161ffb903f91c3711fc89e91ea7ed
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-09-26 16:34:56 +0400

    bug fixing

--- initialisation/03.1_LOAD_FSJ_CODE_LISTS.R
+++ initialisation/03.1_LOAD_FSJ_CODE_LISTS.R
@@ -1,7 +1,10 @@
 print("Loading the code lists for FSJ data...")
 
 # Read the code lists
-CL_COUNTRIES = read_csv("../inputs/data/FSJ/CL_FI_COUNTRY_GROUPS.csv") %>% dplyr::select(UN_CODE = UN_Code, COUNTRY_CODE = ISO3_Code, COUNTRY = Name_En)
+CL_COUNTRIES = read_csv("../inputs/data/FSJ/CL_FI_COUNTRY_GROUPS.csv") %>% 
+  dplyr::select(UN_CODE = UN_Code, COUNTRY_CODE = ISO3_Code, COUNTRY = Name_En) %>% 
+  mutate(UN_CODE = as.integer(UN_CODE))
+CL_COUNTRIES <- as.data.table(CL_COUNTRIES)
 
 CL_SPECIES = fread("../inputs/data/FSJ/CL_FI_SPECIES_GROUPS.csv", encoding = "UTF-8")[, .(SPECIES_CODE = `3A_Code`, SPECIES = Name_En, SPECIES_SCIENTIFIC = Scientific_Name, ISSCAAP_NAME = ISSCAAP_Group_En)]
 

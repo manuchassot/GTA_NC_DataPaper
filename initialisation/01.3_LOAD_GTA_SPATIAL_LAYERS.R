07129cd4bccb98bc8057e0445931af84ab9ee22c
manuchassot <manu.chassot@gmail.com>
2023-11-24 19:51:42 +0400

    Cleaning everything to nothing

--- initialisation/01.3_LOAD_GTA_SPATIAL_LAYERS.R
+++
@@ -1,9 +0,0 @@
-print("Loading the spatial layers...")
-
-## World borders ####
-WORLD_BORDERS_SF = ne_countries(returnclass = "sf")
-
-## Countries and tRFMO areas
-base::load("../inputs/spatial_layers/SpatialLayers.RData")
-
-print("Spatial layers loaded!")
